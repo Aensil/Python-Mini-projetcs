241efdc83e6de9494b01e0c98077ec703b7578a0
Abdel Silva <aensilesqla@gmail.com>
2022-10-31 00:13:16 -0500

    Changes test1

---
+++ Tets.py
@@ -0,0 +1,7 @@
+x = ['node', 'react', 'vuel']
+print(x)
+
+for i in x:
+    x.append(i.upper())
+
+print(x)
--- examen.py
+++ examen.py
@@ -4,12 +4,12 @@ def calPoints(obs)->int:
     for i in range (len(ops)):
         if ops[i]=='+':
             try:
-                L.append(float(L[-1]) + float(L[-2]))
+                L.append(int(L[-1]) + int(L[-2]))
             except:
                 print('For + there any 2 last number')
         elif ops[i]=='D':
             try:
-                L.append(float(L[-1])*2)
+                L.append(int(L[-1])*2)
             except:
                 print('for D there any last score')
         elif ops[i]=='C':
@@ -19,15 +19,16 @@ def calPoints(obs)->int:
                 print("For C there any previous score")
         else:
             try:
-                L.append(float(ops[i]))
+                L.append(int(ops[i]))
             except:
                 print('only print number')
-    result=0.0
+    result = 0
     for i in range(len(L)):
-        result=result + float(L[i])
+        result=result + L[i]
     return result
 if __name__=='__main__':
     line=input()
     ops=line.strip().split()
-    calPoints(ops)
+    #ops=["5","2","C","D","+"]
+    #ops=["5","-2","4","C","D","9","+","+"]
     print(calPoints(ops))
